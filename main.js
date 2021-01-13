566d7dc6bf7cb7c69fc1e66875589c8e97307104
Luan Teixeira <luanlts@outlook.com>
2021-01-13 11:38:38 -0300

    LUAN: Arrays (definição, declaração e exemplos)

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ function somar(evento) {
     }
 }
 
+// * .keyCode == codigo pega a tecla que o usuario apertou
+// * .ctrlKey == bool verifica se ele esta pressionou o ctrl junto
+
 function red(){
     clear();
-    document.getElementById("title").classList.add('red');
+    document.getElementById("title").classList.add('red'); //! classList.add adiciona uma classe ao elemento html
 }
-
 function green(){
     clear();
     document.getElementById("title").classList.add('green');
@@ -26,9 +28,48 @@ function clear() {
     document.getElementById("title").classList.remove("blue");
 }
 
-//.style.qualquer propriedade do CSS
+//.style.qualquer propriedade do CSS sem "-"
 
 function showPhone(element) {
     element.style.display="none";
     document.getElementById("telefone").style.display = "block";
 }
+
+/*
+    ?const nome = "luan" -> o valor de nome pode ser alterado mas a sua estrutura não
+    ?const pessoa = { 
+    ?    nome : "Luan",
+    ?    sobrenome : "Teixeira"
+    ?}
+    *pessoa.nome = "otavio" // True
+    !pessoa = {
+    !    apelido : "doido",
+    !    idade : 20
+    !} // False
+*/
+
+/* Evento de teclado 
+    *onkeyup="funcao(event)"// evento acontece quando o usuário solta a tecla
+    *onkeydown="funcao(event)"// evento acontece quando o usuário abaixa a tecla 
+    *onkeypress="funcao(event)"// evento acontece quando o usuario preciona a tecla
+*/
+
+//Arrays
+
+let carros = [
+    'Palio',
+    'Corsa',
+    'Opala',
+    'Maverick',
+    ( nome ) => {carros = [
+        ...carros,
+        nome
+    ]}
+]
+
+carros[4]("Onix")
+carros[4]("Cruze")
+carros[4]("Celta")
+carros[4]("Fusca")
+console.log(carros)
+
